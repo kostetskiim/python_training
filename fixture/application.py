2b3142f1e024df94dc466fcd8fe31f1730a7546f
Home <1@mail.ru>
2020-11-19 18:55:21 +0400

    Задание 16

--- fixture/application.py
+++ fixture/application.py
@@ -5,13 +5,20 @@ from fixture.contact import ContactHelper
 
 
 class Application:
-    def __init__(self):
-        self.wd = webdriver.Firefox()
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "edge":
+            self.wd = webdriver.Edge()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
         self.wd.implicitly_wait(10)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
-
+        self.base_url = base_url
 
     def is_valid(self):
         try:
@@ -29,7 +36,7 @@ class Application:
         # open home page
         wd = self.wd
         wd.get("http://localhost/addressbook/")
-
+        wd.get(self.base_url)
 
 
 
